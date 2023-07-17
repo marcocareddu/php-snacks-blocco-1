e4bbea5e29eb60129efcdc98b1b26ddd61e7e11a
marcocareddu <saldumaccu@tuta.io>
2023-07-17 15:04:03 +0200

    PHP Snack 1 - Added Array

---
+++ snack_1.php
@@ -0,0 +1,51 @@
+<!-- # Snack 1
+Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni partita avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema:
+Olimpia Milano - Cantù | 55-60 -->
+
+<?php
+
+// Create array
+$match = [
+    [
+        "home" => [
+            "team" => "Milano",
+            "score" => 92,
+        ],
+        "away" => [
+            "team" => "Virtus Bologna",
+            "score" => 82,
+        ],
+    ],
+    [
+        "home" => [
+            "team" => "Sassari",
+            "score" => 95,
+        ],
+        "away" => [
+            "team" => "Tortona",
+            "score" => 82,
+        ],
+    ],
+    [
+        "home" => [
+            "team" => "Pesaro",
+            "score" => 104,
+        ],
+        "away" => [
+            "team" => "Brindisi",
+            "score" => 80,
+        ],
+    ],
+    [
+        "home" => [
+            "team" => "Trento",
+            "score" => 79,
+        ],
+        "away" => [
+            "team" => "Venezia",
+            "score" => 69,
+        ],
+    ],
+];
+
+?>
