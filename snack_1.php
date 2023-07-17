d9ad8327c597ed2be64a0ee28868df24a279ec56
marcocareddu <saldumaccu@tuta.io>
2023-07-17 15:19:48 +0200

    PHP Snack 1 - Print result in DOM

--- snack_1.php
+++ snack_1.php
@@ -5,7 +5,7 @@ Olimpia Milano - Cantù | 55-60 -->
 <?php
 
 // Create array
-$match = [
+$matches = [
     [
         "home" => [
             "team" => "Milano",
@@ -47,5 +47,47 @@ $match = [
         ],
     ],
 ];
-
 ?>
+
+
+<!-- HTML -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="./css/style.css" type="text/css">
+    <title>PHP - Snack 1</title>
+</head>
+
+<body>
+    <div class="container d-flex justify-content-center">
+        <div class="mt-3 text-center">
+
+            <h1>PHP SNACK - 1</h1>
+            <div class="pt-3">
+                <div class="pt-3">
+                    <h3>Risultati delle partite di Basket</h3>
+                    <ul class="pt-3">
+
+                        <!-- Foreach element in array -->
+                        <?php foreach ($matches as $match) : ?>
+                            <li>
+
+                                <!-- Print element of array -->
+                                <?= $match["home"]["team"] . ' ' . $match["away"]["team"] . ' | ' . $match["home"]["score"] . '-' . $match["away"]["score"] ?>
+                            </li>
+                        <?php endforeach ?>
+                    </ul>
+                </div>
+            </div>
+        </div>
+</body>
+
+</html>
